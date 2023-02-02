6f4fb48585e8ab373d6ca343b2662512335742a9
rybakN <dnb4949@gmail.com>
2023-02-02 21:16:38 +0300

    feat: start task

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import * as process from 'process';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const PORT = process.env.PORT || 4000;
+  await app.listen(PORT);
 }
 bootstrap();
---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,8 @@
+export class User {
+  d: string; // uuid v4
+  login: string;
+  password: string;
+  version: number; // integer number, increments on update
+  createdAt: number; // timestamp of creation
+  updatedAt: number; // timestamp of last update
+}
