44a557597373a4f67fb1ba680fb56c5a39847415
rybakN <dnb4949@gmail.com>
2023-02-20 16:35:32 +0300

    feat: add migrations

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ COPY package*.json .
 RUN npm install
 COPY . .
 EXPOSE 4000
-CMD ["npm", "run", "watch"]
+CMD ["npm", "run", "start:appindocker"]
--- src/typeOrmConfig/typeOrmConfig.ts
+++ src/typeOrmConfig/typeOrmConfig.ts
@@ -1,14 +1,14 @@
 import { UserEntity } from '../routes/user/entities/user.entity';
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AlbumEntity } from '../routes/albums/entities/album.entity';
 import { TrackEntity } from '../routes/tracks/entities/track.entity';
 import { ArtistEntity } from '../routes/artists/entities/artist.entity';
 import * as process from 'process';
 import * as dotenv from 'dotenv';
+import { DataSourceOptions } from 'typeorm';
 
 dotenv.config();
 
-export const TypeOrmConfig: TypeOrmModuleOptions = {
+export const TypeOrmConfig: DataSourceOptions = {
   type: 'postgres',
   host: process.env.POSTGRES_HOST,
   port: +process.env.POSTGRES_PORT,
@@ -16,5 +16,6 @@ export const TypeOrmConfig: TypeOrmModuleOptions = {
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   entities: [UserEntity, AlbumEntity, TrackEntity, ArtistEntity],
-  synchronize: true,
+  synchronize: false,
+  migrationsRun: true,
 };
