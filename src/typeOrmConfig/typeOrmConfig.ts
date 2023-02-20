d53ac0f51a6a29df79f256f39b194f796f4e192e
rybakN <dnb4949@gmail.com>
2023-02-20 21:25:57 +0300

    refactor: docker-compose.yml

--- src/typeOrmConfig/typeOrmConfig.ts
+++ src/typeOrmConfig/typeOrmConfig.ts
@@ -10,11 +10,7 @@ dotenv.config();
 
 export const TypeOrmConfig: DataSourceOptions = {
   type: 'postgres',
-  host: process.env.POSTGRES_HOST,
-  port: +process.env.POSTGRES_PORT,
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
+  url: process.env.POSTGRES_URL,
   entities: [UserEntity, AlbumEntity, TrackEntity, ArtistEntity],
   synchronize: false,
   migrationsRun: true,
