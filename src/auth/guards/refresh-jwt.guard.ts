4453767234c4f41441608ff97171ba2c58b0b1aa
rybakN <dnb4949@gmail.com>
2023-02-27 20:43:13 +0300

    fix: status code in refresh endpoint

--- src/auth/guards/refresh-jwt.guard.ts
+++ src/auth/guards/refresh-jwt.guard.ts
@@ -1,5 +1,5 @@
 import { AuthGuard } from '@nestjs/passport';
-import { BadRequestException, ExecutionContext } from '@nestjs/common';
+import { ExecutionContext, UnauthorizedException } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { Request } from 'express';
 
@@ -14,7 +14,7 @@ export class RefreshJwtGuard extends AuthGuard('RefreshJwt') {
     const req: Request = context.switchToHttp().getRequest();
     const { refreshToken } = req.body;
     if (refreshToken == undefined || typeof refreshToken !== 'string')
-      throw new BadRequestException('No refresh token or it not a strings');
+      throw new UnauthorizedException('No refresh token or it not a strings');
     return super.canActivate(context);
   }
 }
