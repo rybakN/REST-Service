a4c2238e28c1251cd2778791522279b6cf3a6fe6
rybakN <dnb4949@gmail.com>
2023-02-27 21:48:01 +0300

    fix: status code invalid refresh token

--- src/auth/guards/refresh-jwt.guard.ts
+++ src/auth/guards/refresh-jwt.guard.ts
@@ -1,7 +1,13 @@
 import { AuthGuard } from '@nestjs/passport';
-import { ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import {
+  ExecutionContext,
+  ForbiddenException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { Request } from 'express';
+import * as process from 'process';
+import * as jwt from 'jsonwebtoken';
 
 export class RefreshJwtGuard extends AuthGuard('RefreshJwt') {
   constructor() {
@@ -13,8 +19,15 @@ export class RefreshJwtGuard extends AuthGuard('RefreshJwt') {
   ): boolean | Promise<boolean> | Observable<boolean> {
     const req: Request = context.switchToHttp().getRequest();
     const { refreshToken } = req.body;
+
     if (refreshToken == undefined || typeof refreshToken !== 'string')
       throw new UnauthorizedException('No refresh token or it not a strings');
+
+    try {
+      jwt.verify(refreshToken, process.env.JWT_SECRET_REFRESH_KEY);
+    } catch {
+      throw new ForbiddenException('Refresh token is invalid or expired');
+    }
     return super.canActivate(context);
   }
 }
